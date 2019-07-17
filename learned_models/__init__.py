b021eb8ab3a8cc1bd135279214a7a3dcdd1a45e4
jbloom <profjsb@gmail.com>
2019-07-17 07:15:18 -0700

    refactored learned model configuration

---
+++ learned_models/__init__.py
@@ -0,0 +1,13 @@
+from deepCR.unet import UNet2Sigmoid, UNet3, UNet2
+from os import path
+
+__all__ = ('mask_dict', 'inpaint_dict')
+
+mask_dict = {'ACS-WFC-F606W-2-32': [UNet2Sigmoid, (1, 1, 32)],
+             'ACS-WFC-F606W-2-4': [UNet2Sigmoid, (1, 1, 4)]}
+
+inpaint_dict = {'ACS-WFC-F606W-3-32': [UNet3, (2, 1, 32)],
+                'ACS-WFC-F606W-2-32': [UNet2, (2, 1, 32)],
+                'ACS-WFC-F606W-2-8': [UNet2, (2, 1, 8)]}
+
+default_model_path = path.join(path.dirname(__file__))
