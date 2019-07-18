77ff4fdce1cadb3a310096060f42ed76440a9d23
jbloom <profjsb@gmail.com>
2019-07-18 13:47:16 -0700

    added parallel support for clean

--- deepCR/test/test_model.py
+++ deepCR/test/test_model.py
@@ -1,18 +1,42 @@
-import pytest
+import time
 
 import numpy as np
+import pytest
 
 from .. import model
 
 
-def test_deepCR():
+def test_deepCR_serial():
 
     mdl = model.deepCR(mask='ACS-WFC-F606W-2-32', device='CPU')
     in_im = np.ones((256, 256))
+    t0 = time.time()
     out = mdl.clean(in_im)
     assert (out[0].shape, out[1].shape) == (in_im.shape, in_im.shape)
+
     out = mdl.clean(in_im, inpaint=False)
     assert out.shape == in_im.shape
+
+def test_deepCR_parallel():
+
+    mdl = model.deepCR(mask='ACS-WFC-F606W-2-32', device='CPU')
+    in_im = np.ones((512, 512))
+
+    out = mdl.clean(in_im, parallel=True, seg=256, n_jobs=-1)
+    assert (out[0].shape, out[1].shape) == (in_im.shape, in_im.shape)
+
+    # Is the serial runtime slower than the parallel runtime on a big image?
+    in_im = np.ones((3096, 2000))
+    t0 = time.time()
+    out = mdl.clean(in_im, inpaint=False, parallel=True, seg=256)
+    par_runtime = time.time() - t0
+    assert out.shape == in_im.shape
+
+    t0 = time.time()
+    out = mdl.clean(in_im, inpaint=False, parallel=False, seg=256)
+    ser_runtime = time.time() - t0
+    # assert False, f"par={par_runtime}, ser={ser_runtime}"
+    assert par_runtime < ser_runtime
 
 
 def test_seg():
