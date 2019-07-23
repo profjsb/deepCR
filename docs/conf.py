9516735d27f1e13a841b672afeb46b052435483e
jbloom <profjsb@gmail.com>
2019-07-23 00:20:29 -0700

    mocking torch to avoid memory error in RTD

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+autodoc_mock_imports = ["torch"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
