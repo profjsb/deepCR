32c17a385b26c5c78d8aef66fe06779c58a24fff
Keming Zhang <keming.zhang@columbia.edu>
2019-08-02 10:14:26 +0300

    fix slice conventions

--- deepCR/dataset.py
+++ deepCR/dataset.py
@@ -3,13 +3,16 @@ from torch.utils.data import Dataset
 
 
 class dataset(Dataset):
-    def __init__(self, image, mask, ignore=None, sky=None, aug_sky=[0, 0], part='train', f_val=0.1, seed=1):
+    def __init__(self, image, mask, ignore=None, sky=None, aug_sky=[0, 0], part=None, f_val=0.1, seed=1):
         """ custom pytorch dataset class to load deepCR-mask training data
         :param image: image with CR
         :param mask: CR mask
         :param ignore: loss mask, e.g., bad pixel, saturation, etc.
         :param sky: (np.ndarray) [N,] sky background level
-        :param aug_sky: [negative number, positive number]. Add sky background by aug_sky[0] * sky to aug_sky[1] * sky.
+        :param aug_sky: [float, float]. If sky is provided, use random sky background in the range
+          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
+          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
+          training set is discrete and limited.
         :param part: either 'train' or 'val'. split by 0.8, 0.2
         :param f_val: percentage of dataset reserved as validation set.
         :param seed: fix numpy random seed to seed, for reproducibility.
@@ -17,27 +20,25 @@ class dataset(Dataset):
 
         np.random.seed(seed)
         len = image.shape[0]
-        assert f_val < 1
+        assert f_val < 1 and f_val > 0
         f_train = 1 - f_val
         if sky is None:
             sky = np.zeros_like(image)
         if ignore is None:
             ignore = np.zeros_like(image)
+
         if part == 'train':
-            self.image = image[np.s_[:int(len * f_train)]]
-            self.mask = mask[np.s_[:int(len * f_train)]]
-            self.ignore = ignore[np.s_[:int(len * f_train)]]
-            self.sky = sky[np.s_[:int(len * f_train)]]
+            slice = np.s_[:int(len * f_train)]
         elif part == 'val':
-            self.image = image[np.s_[int(len * f_train):]]
-            self.mask = mask[np.s_[int(len * f_train):]]
-            self.ignore = ignore[np.s_[int(len * f_train):]]
-            self.sky = sky[np.s_[int(len * f_train):]]
+            slice = np.s_[int(len * f_train):]
         else:
-            self.image = image
-            self.mask = mask
-            self.ignore = ignore
-            self.sky = sky
+            slice = np.s_[0:]
+
+        self.image = image[slice]
+        self.mask = mask[slice]
+        self.ignore = ignore[slice]
+        self.sky = sky[slice]
+
         self.aug_sky = aug_sky
 
     def __len__(self):
--- deepCR/test/test_dataset.py
+++ deepCR/test/test_dataset.py
@@ -1,17 +1,20 @@
 import numpy as np
 import pytest
 
-from .. import dataset
+from deepCR.dataset import dataset
 
 
 def test_dataset():
     inputs = np.random.rand(10,32,32); sky = np.random.rand(10)
-    data = dataset.dataset(image=inputs, mask=inputs, ignore=inputs, sky=sky, part='train', aug_sky=[1, 1], f_val=0.1)
+    data = dataset(image=inputs, mask=inputs, ignore=inputs, sky=sky, part='train', aug_sky=[1, 1], f_val=0.1)
     data0 = data[0]
     assert len(data) == 9
-    assert (data0[0] == inputs[0] + sky[0]).all()
+    assert (data0[0] == (inputs[0] + sky[0])).all()
     assert (data0[1] == inputs[0]).all()
     assert (data0[2] == inputs[0]).all()
+
+    data = dataset(image=inputs, mask=inputs)
+    assert len(data) == 10
 
 
 if __name__ == '__main__':
