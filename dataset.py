8422c8d8285b768ec69c7910239aae02099854c2
Kyubin James Kwon <kgb0255@berkeley.edu>
2020-09-08 18:14:31 -0700

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,235 @@
+import numpy as np
+from torch.utils.data import Dataset
+import os
+
+__all__ = ['dataset', 'DatasetSim']
+
+
+class DatasetSim(Dataset):
+    def __init__(self, image, cr, sky=None, aug_sky=(0, 0), aug_img=(1, 1), noise=False, saturation=1e5,
+                 n_mask=1, norm=False, percentile_limit=50, part=None, f_val=0.1, seed=1):
+        """ custom pytorch dataset class to load deepCR-mask training data
+        :param image: list of complete path to npy arrays, each containing one 2D image
+        :param cr: list of complete path to npy arrays, each containing one 2D mask
+        :param sky: np.ndarray [N,] or float, sky background level
+        :param aug_sky: (float, float). If sky is provided, use random sky background in the range
+          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
+          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
+          training set is discrete and limited.
+        :param aug_img: (float, float). Multiply image (after sky augmentation) by a factor within that range
+          with a log probability prior.
+        :param noise: bool. Whether to add Poisson noise to image.
+        :param saturation: float. maximum pixel value.
+        :param n_mask: number of mask to stack for each image
+        :param norm: bool. Whether to scale generated image by the standard deviation and median of
+         part of the image below percentile_limit.
+        :param percentile_limit: float. Upper percentile limit to calculate std and median, when norm=True
+        :param part: either 'train' or 'val'. split by 0.8, 0.2
+        :param f_val: percentage of dataset reserved as validation set.
+        :param seed: fix numpy random seed to seed, for reproducibility.
+        """
+
+        np.random.seed(seed)
+        self.len_image = len(image)
+        self.len_mask = len(cr)
+        self.n_mask = n_mask
+        self.noise = noise
+        self.saturation = saturation
+
+        assert f_val < 1 and f_val > 0
+        f_train = 1 - f_val
+        if part == 'train':
+            s = np.s_[:int(self.len_image * f_train)]
+            s_cr = np.s_[:int(self.len_mask * f_train)]
+        elif part == 'val':
+            s = np.s_[int(self.len_image * f_train):]
+            s_cr = np.s_[int(self.len_mask * f_train):]
+        else:
+            s = np.s_[0:]
+            s_cr = np.s_[0:]
+
+        if sky is None:
+            sky = np.zeros(self.len_image)
+        elif type(sky) != np.ndarray:
+            sky = np.array([sky]*self.len_image)
+
+        self.image = image[s]
+        self.cr = cr[s_cr]
+        self.sky = sky[s]
+        self.aug_sky = aug_sky
+        self.aug_img = (np.log10(aug_img[0]), np.log10(aug_img[1]))
+        self.norm = norm
+        self.percentile_limit = percentile_limit
+
+        self.len_image = len(self.image)
+        self.len_mask = len(self.cr)
+
+    def get_cr(self):
+        """
+        Generate cosmic ray images from stacked dark frames.
+        Sample self.n_mask number of cr masks and add them together.
+        :return: (cr_img, cr_mask): sampled and added dark image containing only cr and accumulative cr mask
+        """
+        cr_id = np.random.randint(0, self.len_mask, self.n_mask)
+        crs = []; masks = []
+        for i in cr_id:
+            arr = np.load(self.cr[i])
+            crs.append(arr[0][None,:])
+            masks.append(arr[1][None,:])
+        masks = np.concatenate(masks).sum(axis=0) > 0
+        crs = np.concatenate(crs).sum(axis=0)
+        return crs, masks
+
+    def get_image(self, i):
+        data = np.load(self.image[i])
+        if len(data.shape) == 3:
+            return data[0], data[1]
+        else:
+            return data, np.zeros_like(data)
+
+    def __len__(self):
+        return self.len_image
+
+    def __getitem__(self, i):
+        cr, mask = self.get_cr()
+        image, ignore = self.get_image(i)
+        f_bkg_aug = self.aug_sky[0] + np.random.rand() * (self.aug_sky[1] - self.aug_sky[0])
+        f_img_aug = self.aug_img[0] + np.random.rand() * (self.aug_img[1] - self.aug_img[0])
+        f_img_aug = 10**f_img_aug
+        bkg = f_bkg_aug * self.sky[i]
+        img = (image + bkg) * f_img_aug + cr
+        scale = img.copy()
+        scale[scale < 1] = 1.
+        scale = scale**0.5
+        if self.noise:
+            noise = np.random.normal(0, scale, image.shape)
+        else:
+            noise = np.zeros_like(image)
+        img += noise
+        img[img > self.saturation] = self.saturation
+
+        if self.norm:
+            limit = np.percentile(img, self.percentile_limit)
+            clip = img[img < limit]
+            scale = clip.std()
+            median = np.percentile(clip, 50)
+            img -= median
+            img /= scale
+
+        return img, mask, ignore
+
+
+class dataset(Dataset):
+    def __init__(self, image, mask, ignore=None, sky=None, aug_sky=[0, 0], part=None, f_val=0.1, seed=1):
+        """ custom pytorch dataset class to load deepCR-mask training data
+        :param image: image with CR. Could be (N, W, H) array or list of path to single (W, H) images.
+        :param mask: CR mask. Could be (N, W, H) array or list of path to single (W, H) images.
+        :param ignore: (optional) loss mask, e.g., bad pixel, saturation, etc. Could be (N, W, H) array or list
+        of path to single (W, H) images
+        :param sky: (np.ndarray) [N,] sky background level
+        :param aug_sky: [float, float]. If sky is provided, use random sky background in the range
+          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
+          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
+          training set is discrete and limited.
+        :param part: either 'train' or 'val'. split by 0.8, 0.2
+        :param f_val: percentage of dataset reserved as validation set.
+        :param seed: fix numpy random seed to seed, for reproducibility.
+        """
+
+        np.random.seed(seed)
+        len = image.shape[0]
+        assert f_val < 1 and f_val > 0
+        f_train = 1 - f_val
+        if sky is None:
+            sky = np.zeros_like(image)
+        if ignore is None:
+            ignore = np.zeros_like(image)
+
+        if part == 'train':
+            s = np.s_[:max(1, int(len * f_train))]
+        elif part == 'val':
+            s = np.s_[min(len - 1, int(len * f_train)):]
+        else:
+            s = np.s_[0:]
+
+        self.image = image[s]
+        self.mask = mask[s]
+        self.ignore = ignore[s]
+        self.sky = sky[s]
+
+        self.aug_sky = aug_sky
+
+    def __len__(self):
+        return self.image.shape[0]
+
+    def __getitem__(self, i):
+        a = (self.aug_sky[0] + np.random.rand() * (self.aug_sky[1] - self.aug_sky[0])) * self.sky[i]
+        ignore = self.ignore[i] if type(self.ignore[i]) != str else np.load(self.ignore[i])
+        if type(self.image[i]) == str:
+            return np.load(self.image[i]) + a, np.load(self.mask[i]), ignore
+        else:
+            return self.image[i] + a, self.mask[i], ignore
+
+
+class PairedDatasetImagePath(Dataset):
+    def __init__(self, paths, skyaug_min=0, skyaug_max=0, part=None, f_val=0.1, seed=1):
+        """ custom pytorch dataset class to load deepCR-mask training data
+        :param paths: (list) list of file paths to (3, W, H) images: image, cr, ignore.
+        :param skyaug_min: [float, float]. If sky is provided, use random sky background in the range
+          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
+          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
+          training set is discrete and limited.
+        :param skyaug_min: float. subtract maximum amount of abs(skyaug_min) * sky_level as data augmentation
+        :param skyaug_max: float. add maximum amount of skyaug_max * sky_level as data augmentation
+        :param part: either 'train' or 'val'.
+        :param f_val: percentage of dataset reserved as validation set.
+        :param seed: fix numpy random seed to seed, for reproducibility.
+        """
+        assert 0 < f_val < 1
+        np.random.seed(seed)
+        n_total = len(paths)
+        n_train = int(n_total * (1 - f_val)) #int(len * (1 - f_val)) JK
+
+        if part == 'train':
+            s = np.s_[:max(1, n_train)]
+        elif part == 'val':
+            s = np.s_[min(n_total - 1, n_train):]
+        else:
+            s = np.s_[0:]
+
+        self.paths = paths[s]
+        self.skyaug_min = skyaug_min
+        self.skyaug_max = skyaug_max
+
+    def __len__(self):
+        return len(self.paths)
+
+    def get_skyaug(self, i):
+        """
+        Return the amount of background flux to be added to image
+        The original sky background should be saved in sky.npy in each sub-directory
+        Otherwise always return 0
+        :param i: index of file
+        :return: amount of flux to add to image
+        """
+        path = os.path.split(self.paths[i])[0]
+        sky_path = os.path.join(path, 'sky.npy') #JK
+        if os.path.isfile(sky_path):
+            f_img = self.paths[i].split('/')[-1]
+            sky_idx = int(f_img.split('_')[0])
+            sky = np.load(sky_path)[sky_idx-1]
+            return sky * np.random.uniform(self.skyaug_min, self.skyaug_max)
+        else:
+            return 0
+
+    def __getitem__(self, i):
+        data = np.load(self.paths[i])
+        image = data[0]
+        mask = data[1]
+        if data.shape[0] == 3:
+            ignore = data[2]
+        else:
+            ignore = np.zeros_like(data[0])
+        # try:#JK
+        skyaug = self.get_skyaug(i)
+        return image + skyaug, mask, ignore
---
+++ training.py
@@ -0,0 +1,313 @@
+""" module for training new deepCR-mask models
+"""
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from tqdm import tqdm as tqdm
+from tqdm import tqdm_notebook as tqdm_notebook
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.tensorboard import SummaryWriter
+
+from deepCR.util import maskMetric
+from deepCR.dataset import dataset, DatasetSim, PairedDatasetImagePath
+from deepCR.unet import WrappedModel, UNet2Sigmoid
+
+__all__ = 'train'
+
+
+class VoidLRScheduler:
+    def __init__(self):
+        pass
+
+    def _reset(self):
+        pass
+
+    def step(self):
+        pass
+
+
+class train:
+
+    def __init__(self, image, mask=None, ignore=None, sky=None, mode='pair', aug_sky=(0, 0), aug_img=(1, 1), noise=False, saturation=1e5,
+                 n_mask_train=1, n_mask_val=1, norm=False, percentile_limit=50, name='model', hidden=32, epoch=50,
+                 epoch_phase0=None, batch_size=16, lr=0.005, auto_lr_decay=True, lr_decay_patience=4,
+                 lr_decay_factor=0.1, save_after=1e5, plot_every=10, verbose=True, use_tqdm=False,
+                 use_tqdm_notebook=False, directory='./'):
+
+        """ This is the class for training deepCR-mask.
+        :param image: np.ndarray (N*W*W) of CR affected images or list of npy arrays of shape (2or3,W,W), where
+        the 1st dim is CR affect image, 2nd dim is CR mask, (optional) 3rd dimension is ignore mask.
+        See documentation: training tutorial.
+        :param mask: np.ndarray (N*W*W) training data: CR mask array.
+        :param ignore: training data: Mask for taking loss. e.g., bad pixel, saturation, etc.
+        :param sky: np.ndarray (N,) (optional) sky background. When param: iamge is list to npy files, provide sky level
+        as sky.npy in each image subdirectory (see documentation: training tutorial).
+        :param aug_sky: [float, float]. If sky is provided, use random sky background in the range
+          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
+          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
+          training set is discrete and limited.
+        :param n_mask: number of dark images to sample to create one cosmic ray image and mask
+        :param name: model name. model saved to name_epoch.pth
+        :param hidden: number of channels for the first convolution layer. default: 50
+        :param gpu: True if use GPU for training
+        :param epoch: Number of epochs to train. default: 50
+        :param batch_size: training batch size. default: 16
+        :param lr: learning rate. default: 0.005
+        :param auto_lr_decay: reduce learning rate by "lr_decay_factor" after validation loss do not decrease for
+          "lr_decay_patience" + 1 epochs.
+        :param lr_decay_patience: reduce learning rate by lr_decay_factor after validation loss do not decrease for
+          "lr_decay_patience" + 1 epochs.
+        :param lr_decay_factor: multiplicative factor by which to reduce learning rate.
+        :param save_after: epoch after which trainer automatically saves model state with lowest validation loss
+        :param plot_every: for every "plot_every" epoch, plot mask prediction and ground truth for 1st image in
+          validation set.
+        :param verbose: print validation loss and detection rates for every epoch.
+        :param use_tqdm: whether to show tqdm progress bar.
+        :param use_tqdm_notebook: whether to use jupyter notebook version of tqdm. Overwrites tqdm_default.
+        :param directory: directory relative to current path to save trained model.
+        """
+        if torch.cuda.is_available():
+            gpu = True
+        else:
+            gpu = False
+            print('No GPU detected on this device! Training on CPU.')
+
+        if sky is None and aug_sky != (0, 0):
+            raise AttributeError('Var (sky) is required for sky background augmentation!')
+        if ignore is None:
+            ignore = np.zeros_like(image)
+        if mode == 'pair':
+            if type(image[0]) == str: #or type(image[0]) == np.str_:
+                data_train = PairedDatasetImagePath(image, aug_sky[0], aug_sky[1], part='train')
+                data_val = PairedDatasetImagePath(image, aug_sky[0], aug_sky[1], part='val')
+            else:
+                data_train = dataset(image, mask, ignore, sky, part='train', aug_sky=aug_sky)
+                data_val = dataset(image, mask, ignore, sky, part='val', aug_sky=aug_sky)
+        elif mode == 'simulate':
+            data_train = DatasetSim(image, mask, sky, aug_sky=aug_sky, aug_img=aug_img, saturation=saturation,
+                                    norm=norm, percentile_limit=percentile_limit, part='train', noise=noise, n_mask=n_mask_train)
+            data_val = DatasetSim(image, mask, sky, aug_sky=aug_sky, aug_img=aug_img, saturation=saturation,
+                                  norm=norm, percentile_limit=percentile_limit, part='val', noise=noise, n_mask=n_mask_val)
+        else:
+            raise TypeError('Mode must be one of pair or simulate!')
+
+        self.TrainLoader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=8)
+        self.ValLoader = DataLoader(data_val, batch_size=batch_size, shuffle=False, num_workers=8)
+        self.shape = data_train[0][0].shape[1]
+        self.name = name
+
+        if gpu:
+            self.dtype = torch.cuda.FloatTensor
+            self.dint = torch.cuda.ByteTensor
+            self.network = nn.DataParallel(UNet2Sigmoid(1,1,hidden))
+            self.network.type(self.dtype)
+        else:
+            self.dtype = torch.FloatTensor
+            self.dint = torch.ByteTensor
+            self.network = WrappedModel(UNet2Sigmoid(1,1,hidden))
+            self.network.type(self.dtype)
+
+        self.optimizer = optim.Adam(self.network.parameters(), lr=lr)
+        if auto_lr_decay:
+            self.lr_scheduler = ReduceLROnPlateau(self.optimizer, factor=lr_decay_factor, patience=lr_decay_patience,
+                                                  cooldown=2, verbose=True, threshold=0.005)
+        else:
+            self.lr_scheduler = VoidLRScheduler()
+        self.lr = lr
+        self.BCELoss = nn.BCELoss()
+        self.validation_loss = []
+        self.epoch_mask = 0
+        self.save_after = save_after
+        self.n_epochs = epoch
+        if epoch_phase0 is None:
+            self.n_epochs_phase0 = int(self.n_epochs * 0.4 + 0.5)
+        else:
+            self.n_epochs_phase0 = epoch_phase0
+        self.every = plot_every
+        self.directory = directory
+        self.verbose = verbose
+        self.mode0_complete = False
+
+        if use_tqdm_notebook:
+            self.tqdm = tqdm_notebook
+        else:
+            self.tqdm = tqdm
+        self.disable_tqdm = not (use_tqdm_notebook or use_tqdm)
+
+        # tensorboard to record and visualize training log (require pytorch 1.1 and up)
+        self.writer = SummaryWriter(log_dir=directory)
+
+    def set_input(self, img0, mask, ignore):
+        """
+        :param img0: input image
+        :param mask: CR mask
+        :param ignore: loss mask
+        :return: None
+        """
+        self.img0 = Variable(img0.type(self.dtype)).view(-1,1, self.shape, self.shape)
+        self.mask = Variable(mask.type(self.dtype)).view(-1,1, self.shape, self.shape)
+        self.ignore = 1- (Variable(ignore.type(self.dtype)).view(-1,1, self.shape, self.shape) == 0).type(torch.uint8)
+
+    def validate_mask(self, epoch=None):
+        """
+        :return: validation loss. print TPR and FPR at threshold = 0.5.
+        """
+        torch.random.manual_seed(0)
+        np.random.seed(0)
+        lmask = 0; count = 0
+        metric = np.zeros(4)
+        for i, dat in enumerate(self.ValLoader):
+            n = dat[0].shape[0]
+            count += n
+            self.set_input(*dat)
+            self.pdt_mask = self.network(self.img0)
+            loss = self.backward_network()
+            lmask += float(loss.detach()) * n
+            metric += maskMetric(self.pdt_mask.reshape(-1, self.shape, self.shape).detach().cpu().numpy() > 0.5, dat[1].numpy())
+        lmask /= count
+        TP, TN, FP, FN = metric[0], metric[1], metric[2], metric[3]
+        TPR = TP / (TP + FN)
+        FPR = FP / (FP + TN)
+        if self.verbose:
+            print('[TPR=%.3f, FPR=%.3f] @threshold = 0.5' % (TPR, FPR))
+        if epoch:
+            self.writer.add_scalar('TPR', TPR, epoch)
+            self.writer.add_scalar('FPR', FPR, epoch)
+            self.writer.add_scalar('validate_loss', lmask, epoch)
+
+        return (lmask)
+
+    def train(self):
+        """ call this function to start training network
+        :return: None
+        """
+        if self.verbose:
+            print('Begin first {} epochs of training'.format(self.n_epochs_phase0))
+            print('Use batch statistics for batch normalization; keep running statistics to be used in phase1')
+            print('')
+        self.train_phase0(self.n_epochs_phase0)
+
+        filename = self.save()
+        self.load(filename)
+        self.optimizer = optim.Adam(self.network.parameters(), lr=self.lr/2.5)
+
+        if self.verbose:
+            print('Continue onto next {} epochs of training'.format(self.n_epochs - self.n_epochs_phase0))
+            print('Batch normalization running statistics frozen and used')
+            print('')
+        self.train_phase1(self.n_epochs - self.n_epochs_phase0)
+
+    def train_phase0(self, epochs):
+        self.network.train()
+        for epoch in self.tqdm(range(epochs), disable=self.disable_tqdm):
+            for t, dat in enumerate(self.TrainLoader):
+                self.optimize_network(dat)
+            self.epoch_mask += 1
+
+            if self.epoch_mask % self.every == 0:
+                self.plot_example()
+
+            if self.verbose:
+                print('----------- epoch = %d -----------' % self.epoch_mask)
+            val_loss = self.validate_mask(epoch)
+            self.validation_loss.append(val_loss)
+            if self.verbose:
+                print('loss = %.4f' % (self.validation_loss[-1]))
+            if (np.array(self.validation_loss)[-1] == np.array(
+                    self.validation_loss).min() and self.epoch_mask > self.save_after):
+                filename = self.save()
+                if self.verbose:
+                    print('Saved to {}.pth'.format(filename))
+            self.lr_scheduler.step(self.validation_loss[-1])
+            if self.verbose:
+                print('')
+
+    def train_phase1(self, epochs):
+        self.set_to_eval()
+        self.lr_scheduler._reset()
+        for epoch in self.tqdm(range(epochs), disable=self.disable_tqdm):
+            for t, dat in enumerate(self.TrainLoader):
+                self.optimize_network(dat)
+            self.epoch_mask += 1
+
+            if self.epoch_mask % self.every==0:
+                self.plot_example()
+
+            if self.verbose:
+                print('----------- epoch = %d -----------' % self.epoch_mask)
+            valLossMask = self.validate_mask(epoch)
+            self.validation_loss.append(valLossMask)
+            if self.verbose:
+                print('loss = %.4f' % (self.validation_loss[-1]))
+            if (np.array(self.validation_loss)[-1] == np.array(
+                    self.validation_loss).min() and self.epoch_mask > self.save_after):
+                filename = self.save()
+                if self.verbose:
+                    print('Saved to {}.pth'.format(filename))
+            self.lr_scheduler.step(self.validation_loss[-1])
+            if self.verbose:
+                print('')
+
+    def plot_example(self):
+        plt.figure(figsize=(10, 30))
+        plt.subplot(131)
+        plt.imshow(np.log(self.img0[0, 0].detach().cpu().numpy()), cmap='gray')
+        plt.title('epoch=%d' % self.epoch_mask)
+        plt.subplot(132)
+        plt.imshow(self.pdt_mask[0, 0].detach().cpu().numpy() > 0.5, cmap='gray')
+        plt.title('prediction > 0.5')
+        plt.subplot(133)
+        plt.imshow(self.mask[0, 0].detach().cpu().numpy(), cmap='gray')
+        plt.title('ground truth')
+        plt.show()
+
+    def set_to_eval(self):
+        self.network.eval()
+
+    def optimize_network(self, dat):
+        self.set_input(*dat)
+        self.pdt_mask = self.network(self.img0)
+        self.optimizer.zero_grad()
+        loss = self.backward_network()
+        loss.backward()
+        self.optimizer.step()
+
+    def backward_network(self):
+        loss = self.BCELoss(self.pdt_mask * (1 - self.ignore), self.mask * (1 - self.ignore))
+        return loss
+
+    def plot_loss(self):
+        """ plot validation loss vs. epoch
+        :return: None
+        """
+        plt.figure(figsize=(10,5))
+        plt.plot(range(self.epoch_mask), self.validation_loss)
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.title('Validation loss')
+        plt.show()
+
+    def save(self):
+        """ save trained network parameters to date_model_name_epoch*.pth
+        :return: None
+        """
+        time = datetime.datetime.now()
+        time = str(time)[:10]
+        filename = '%s_%s_epoch%d' % (time, self.name, self.epoch_mask)
+        torch.save(self.network.state_dict(), self.directory + filename + '.pth')
+        return filename
+
+    def load(self, filename):
+        """ Continue training from a previous model state saved to filename
+        :param filename: (str) filename (without ".pth") to load model state
+        :return: None
+        """
+        self.network.load_state_dict(torch.load(self.directory + filename + '.pth'))
+        loc = filename.find('epoch') + 5
+        self.epoch_mask = int(filename[loc:])
